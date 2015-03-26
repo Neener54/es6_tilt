f63a74fa0bebc928f32b1612a73e65b93ee26bf2
Michael Archibald <micharch54@gmail.com>
2015-03-26 09:16:54 -0600

    fixing registers

--- config/initializers/sprockets.rb
+++ config/initializers/sprockets.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/es6_tilt'
 Rails.application.assets.register_mime_type 'application/javascript', '.js'
-Rails.application.assets.register_engine '.js', Es6Tilt::ES6TiltTransformer
+Rails.application.assets.register_engine '.js', ES6Tilt
 Rails.application.assets.register_mime_type 'application/javascript', '.jsx'
-Rails.application.assets.register_engine '.jsx', Es6Tilt::ES6TiltTransformer
+Rails.application.assets.register_engine '.jsx', ES6Tilt
 Rails.application.assets.register_mime_type 'application/javascript', '.es6'
-Rails.application.assets.register_engine '.es6', Es6Tilt::ES6TiltTransformer
+Rails.application.assets.register_engine '.es6', ES6Tilt
